dfe8c035f41f24e40c236c423ba55b6e8d183076
Liadrid <1506227085@qq.com>
2022-04-08 13:18:57 +0800

    改进设计

--- src/main/java/com/phoenix/free/entity/Message.java
+++ src/main/java/com/phoenix/free/entity/Message.java
@@ -3,16 +3,16 @@ package com.phoenix.free.entity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@NoArgsConstructor
+@SuperBuilder
 @ApiModel("Message 用户私信")
-public class Message extends Post {
+public class Message extends Post{
     @ApiModelProperty("接收者id")
-    private Long userId2;
+    private Long receiverId;
 }
