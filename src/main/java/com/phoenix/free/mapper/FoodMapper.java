50f8333217de2a8f3558913f71f0be4a426eb413
Liadrid <1506227085@qq.com>
2022-07-08 21:34:28 +0800

    重构ExerciseInfo相关接口

--- src/main/java/com/phoenix/free/mapper/FoodMapper.java
+++ src/main/java/com/phoenix/free/mapper/FoodMapper.java
@@ -6,21 +6,4 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FoodMapper extends BaseMapper<Food> {
-//    @Insert("INSERT INTO food(name,category,energy,sugar,fat,protein,cellulose,state) VALUES (#{name},#{category},#{energy},#{sugar},#{fat},#{protein},#{cellulose},#{state}); ")
-//    int insertFoodInfo(Food food);
-//
-//    @Delete("DELETE FROM food WHERE id=#{id};")
-//    int deleteFoodInfoById(Long id);
-//
-//    @Delete("DELETE FROM food WHERE name=#{name};")
-//    int deleteFoodInfoByName(String name);
-//
-//    @ResultType(Food.class)
-//    @Select("SELECT * FROM food WHERE id=#{id};")
-//    Food getFoodInfoById(@Param("id") Long id);
-//
-//    @ResultType(Food.class)
-//    @Select("SELECT * FROM food WHERE name=#{name};")
-//    Food getFoodInfoByName(@Param("name") String name);
-
 }
